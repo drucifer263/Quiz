e0b5dc07cd21dfb78768fa2a8dac5f1366792c53
Drew Watson <tua07182@temple.edu>
2019-05-17 01:48:57 -0400

    first commit

---
+++ Quiz/myJavaScript.js
@@ -0,0 +1,53 @@
+﻿//Code for quiz builder
+$(document).ready(function () {
+
+    function getType() {
+        var selection = $("#quizSelectorList").
+        console.log(selection);
+    }
+    
+    function shortAnswer() {
+
+    }
+
+    function buildSelect() {
+
+    }
+
+    function buildTrueFalse() {
+
+    }
+
+    function buildMultiple() {
+
+    }
+
+    function calculateScore() {
+
+    }
+
+    function textArea() {
+
+    }
+
+    $("#btnAddQuestion").click(function () {
+        var selection = $("#quizSelectorList").val();
+    }); //end of click even handler for btnAddQuestion
+});//end ready
+
+
+
+
+/*JQuery for menu burger*/
+$(document).ready(function () {
+    $('#nav-btn').click(function () {
+        $(this).next().toggle();
+    });
+});
+
+$(window).resize(function () {
+    if ($(window).width() > 600)
+        $('.navList').css("display", "block");
+    else
+        $('.navList').css("display", "none");
+});
